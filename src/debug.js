5381265fdeba444fd2f9845e6fb3ba1149be9979
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-07-20 13:28:09 +0200

    fix debug

--- src/debug.js
+++ src/debug.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 function debug (msg) {
-  fs.appendFile('debug.log', (typeof msg === 'string' ? msg : JSON.stringify(msg)) + '\n', function () {})
+  fs.appendFileSync('debug.log', (typeof msg === 'string' ? msg : JSON.stringify(msg)) + '\n')
 }
 
 module.exports = debug
