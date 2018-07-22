1afd94fc2c32868f38f5e765e854af20cf2e2173
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-07-22 08:43:31 +0200

    add_email: confirm adding entry

--- src/add_email.js
+++ src/add_email.js
@@ -1,4 +1,10 @@
+const ttys = require('ttys')
+const readline = require('readline')
+
 function add_email (db, rows) {
+  let name
+  let email
+
   let stdin = process.stdin
   stdin.setEncoding('utf8')
   let chunks = []
@@ -9,15 +15,33 @@ function add_email (db, rows) {
     lines.forEach(line => {
       let m = line.match(/^From:(.*)<(.*)>/)
       if (m) {
-        let name = m[1].trim()
-        let email = m[2].trim()
-
-        db.set(null, { name, email }, function () {})
-        console.log('New entry: ' + name + ' <' + email + '>')
+        name = m[1].trim()
+        email = m[2].trim()
       }
     })
   })
   stdin.on('end', function () {
+    if (!email) {
+      ttys.stdin.destroy()
+      return console.log('No email address found')
+    }
+
+    let prompts = readline.createInterface({
+      input: ttys.stdin,
+      output: ttys.stdout
+    })
+
+    prompts.question(
+      'Add new entry: ' + name + ' <' + email + '> [Yn]? ',
+      (result) => {
+        if ([ 'y', 'Y', '' ].indexOf(result) !== -1) {
+          db.set(null, { name, email }, function () {})
+        }
+
+        prompts.close()
+        ttys.stdin.destroy()
+      }
+    )
   })
 }
 
