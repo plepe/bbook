f9226dbf224c17d170bb5b4ea596638263f82de2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-07-23 21:58:42 +0200

    Add all fields from abook

--- src/fields.js
+++ src/fields.js
@@ -1,6 +1,19 @@
 module.exports = [
   { id: 'name', title: 'Name', pager: true },
   { id: 'email', title: 'E-Mail address', pager: true },
-  { id: 'phone', title: 'Phone', pager: true },
-  { id: 'country', title: 'Country', pager: false }
+  { id: 'address', title: 'Address', pager: false },
+  { id: 'address2', title: 'Address2', pager: false },
+  { id: 'city', title: 'City', pager: false },
+  { id: 'state', title: 'State/Province', pager: false },
+  { id: 'zip', title: 'ZIP/Postal Code', pager: false },
+  { id: 'country', title: 'Country', pager: false },
+  { id: 'phone', title: 'Home Phone', pager: true },
+  { id: 'workphone', title: 'Work Phone', pager: false },
+  { id: 'fax', title: 'Fax', pager: false },
+  { id: 'mobile', title: 'Mobile', pager: false },
+  { id: 'nick', title: 'Nickname/Alias', pager: false },
+  { id: 'url', title: 'URL', pager: false },
+  { id: 'notes', title: 'Notes', pager: false },
+  { id: 'anniversary', title: 'Anniversary day', pager: false },
+  { id: 'groups', title: 'Groups', pager: false }
 ]
